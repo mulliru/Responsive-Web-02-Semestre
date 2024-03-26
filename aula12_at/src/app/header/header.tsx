4707853e5a5c7783a534226add117470b947dc77
Murillo Ferreira <murilloferreira348@gmail.com>
2024-03-26 08:42:21 -0300

    atualizando aula 12

--- aula12_at/src/app/header/header.tsx
+++ aula12_at/src/app/header/header.tsx
@@ -5,6 +5,7 @@ const Header = () =>{
             <header>
                 <div className="content">
                     <h1>Header</h1>
+                    <p>teste</p>
                 </div>
             </header>
         </>
